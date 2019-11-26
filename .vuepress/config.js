5848012f961b22bc4bb40476772839009bf1ce91
Nick Evans <nick.evans@northwestern.edu>
2019-11-26 11:30:37 -0600

    Skeleton

---
+++ .vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    title: 'AWS for Administrative Systems',
+    description: 'Docs for NUIT AS staff on our cloud implementation.',
+    dest: '.build/docs',
+
+    themeConfig: {
+        repo: 'NIT-Administrative-Systems/AS-CloudDocs',
+        docsDir: 'docs',
+        docsBranch: 'development',
+        editLinks: true,
+        editLinkText: 'Edit Page',
+        lastUpdated: true,
+
+        sidebar: [
+            ['/', 'Introduction'],
+        ],
+    },
+}
